f678ad2c29f4447eb8052f3b48771dda73d9a97a
Md Zahid Hossain <4047397-mzhratul@users.noreply.gitlab.com>
2020-08-16 10:00:17 +0600

    Test PHP Project

---
+++ edit.php
@@ -0,0 +1,62 @@
+<?php include 'header.php'; ?>
+
+<div id="main-content">
+    <h2>Update Record</h2>
+    <?php
+       $conn = mysqli_connect("localhost","root","","crud") or die("Connection Failed");
+
+       $stu_id = $_GET['id'];
+
+       $sql = "SELECT * FROM student WHERE sid={$stu_id}";
+       $result = mysqli_query($conn, $sql) or die("Query Unsuccessful.");
+       if(mysqli_num_rows($result)>0){
+           while($row = mysqli_fetch_assoc($result)){
+    ?>
+    <form class="post-form" action="updatedata.php" method="post">
+      <div class="form-group">
+          <label>Name</label>
+          <input type="hidden" name="sid" value="<?=$row['sid']?>"/>
+          <input type="text" name="sname" value="<?= $row['sname']?>"/>
+      </div>
+      <div class="form-group">
+          <label>Address</label>
+          <input type="text" name="saddress" value="<?= $row['saddress']?>"/>
+      </div>
+      <div class="form-group">
+          <label>Class</label>
+
+        <?php
+        $sql1 = "SELECT * FROM studentclass";
+        $result1 = mysqli_query($conn, $sql1) or die("Query Unsuccessful.");
+        if(mysqli_num_rows($result1)>0){
+            echo '<select name="sclass">
+              <option value="" selected disabled>Select Class</option>';
+            while($row1 = mysqli_fetch_assoc($result1)){
+                if($row['sclass'] == $row1['cid']){
+                    $selected ="selected";
+                }else{
+                    $selected="";
+                }
+                echo "<option $selected value='{$row1["cid"]}'>{$row1["cname"]}</option>";
+            }
+        }
+          echo "</select>";
+          ?>
+      </div>
+      <div class="form-group">
+          <label>Phone</label>
+          <input type="text" name="sphone" value="<?= $row['sphone']?>"/>
+      </div>
+      <input class="submit" type="submit" value="Update"/>
+    </form>
+    <?php
+       }
+       }else{
+           echo "No Record Found";
+       }
+       mysqli_close($conn);
+    ?>
+</div>
+</div>
+</body>
+</html>
